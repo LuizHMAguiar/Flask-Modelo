cbd17f6ea0204adb970640ec162365e25ee2aee5
LuizHMAguiar <1397749@etfbsb.edu.br>
2026-09-03 10:22:37 -0300

    Login funcionando

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'chave-super-secreta'
 
 @app.route("/")
 def login():
@@ -8,15 +9,28 @@ def login():
 
 @app.route("/painel")
 def painel():
-    return render_template("painel.html")
+    if 'usuario_nome' in session:
+        return render_template("painel.html",usuario_nome=session['usuario_nome'],usuario_cpf=session['usuario_cpf'])
+    return redirect('/')
 
 @app.route("/verificar", methods=['POST'])
 def verificar():
     cpf = request.form.get('cpf')
     senha = request.form.get('senha')
     
-    print('Tentando login com: ', cpf,'/senha: ',senha)
+    print('Tentando login com: ', cpf,'/senha: ', senha)
 
+    if cpf == "12345678910" and senha == "123":
+        session['usuario_nome'] = "Gustavo"
+        session['usuario_cpf'] = cpf
+        return redirect('/painel')
+
+    return redirect('/')
+
+@app.route('/sair')
+def sair():
+    session.pop('usuario_nome',None)
+    session.pop('usuario_cpf',None)
     return redirect('/')
 
 if __name__ == "__main__":
